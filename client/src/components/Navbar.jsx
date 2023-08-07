be9484569948bc4f0bbbfc3f92bdd995bcd4c80f
Prachee Nanda <p4nanda@uwaterloo.ca>
2023-08-07 15:27:36 -0400

    Add a better background

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 import StreamRoundedIcon from '@mui/icons-material/StreamRounded';
 
 const Navbar = () => {
   return (
-    <AppBar 
-        position='static'
-        elevation={0}
-        sx={{
-            backgroundColor: 'white',
-            color: '#121212',
-            borderBottom: '1px solid lightgray',
-            fontWeight: 'bold',
-            fontSize: 12,
-        }}
+    <AppBar
+      position="static"
+      elevation={0}
+      sx={{
+        backgroundColor: 'white',
+        color: '#121212',
+        borderBottom: '1px solid lightgray',
+        fontWeight: 'bold',
+        fontSize: 12,
+      }}
     >
-        <Toolbar variant="dense">
-            <StreamRoundedIcon 
-                sx={{
-                    marginLeft: 9
-                }}
-            />
-            <Typography
-                variant="h6"
-                sx={{
-                    marginLeft: 2
-                }}
-            >
-                upwork.
-            </Typography>
-        </Toolbar>
+      <Toolbar variant="dense">
+        <StreamRoundedIcon
+          sx={{
+            marginLeft: 9,
+          }}
+        />
+        <Typography
+          variant="h6"
+          sx={{
+            marginLeft: 2,
+          }}
+        >
+          upwork.
+        </Typography>
+      </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/screens/Landing/Landing.jsx
+++ client/src/screens/Landing/Landing.jsx
@@ -1,36 +1,80 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material';
 import NavigateNextRoundedIcon from '@mui/icons-material/NavigateNextRounded';
-import './Landing.css'
+
+import './Landing.css';
 
 export const Landing = () => {
   return (
-    <Box sx={{marginLeft: 11, marginTop: 15}}>
-      <Typography variant="h2" sx={{
-        fontWeight: 'bold',
-        background: 'linear-gradient(45deg, #1560BD 30%, #002366 90%)',
-        '-webkit-background-clip': 'text',
-        '-webkit-text-fill-color': 'transparent',
-      }}>
-        Generate resources <br></br>
-      </Typography>
-      <Typography variant="h2" sx={{
-        fontWeight: 'bold',
-        color: '#121212'
-      }}>
-        at the speed of lightning!
-      </Typography>
-      {/* This needs to be updated with a description */}
-      <Typography variant="subtitle1" sx={{width: '700px', marginBlock: 2, color: 'gray'}}>
-      Are you preparing for a job interview and seeking valuable resources to enhance your chances of success? This all-in-one website is designed to provide comprehensive support for interview preparation. With upwork, you'll gain access to a wealth of information, company insights, targeted interview questions, and curated reading materials. 
-      </Typography>
-      <Link to="input-description">
-        <Button variant="contained" color="primary" endIcon={<NavigateNextRoundedIcon />}>
-          Get Started
-        </Button>
-      </Link>
-    </Box>
-  )
-}
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center', // Center vertically
+          height: '90vh', // Use full viewport height
+          ml: 12,
+        }}
+      >
+        <Box>
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: 'bold',
+              background: 'linear-gradient(45deg, #1560BD 30%, #002366 90%)',
+              '-webkit-background-clip': 'text',
+              '-webkit-text-fill-color': 'transparent',
+            }}
+          >
+            Generate resources <br></br>
+          </Typography>
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: 'bold',
+              color: '#121212',
+            }}
+          >
+            at the speed of lightning!
+          </Typography>
+          {/* This needs to be updated with a description */}
+          <Typography
+            variant="subtitle1"
+            sx={{ width: '700px', marginBlock: 2, color: 'gray' }}
+          >
+            Are you preparing for a job interview and seeking valuable resources
+            to enhance your chances of success? This all-in-one website is
+            designed to provide comprehensive support for interview preparation.
+            With upwork, you'll gain access to a wealth of information, company
+            insights, targeted interview questions, and curated reading
+            materials.
+          </Typography>
+          <Link to="input-description">
+            <Button
+              variant="outlined"
+              color="primary"
+              endIcon={<NavigateNextRoundedIcon />}
+            >
+              Get Started
+            </Button>
+          </Link>
+        </Box>
+      </Box>
+      <Box>
+        <ul class="background">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </Box>
+    </>
+  );
+};
